7c72874694a09b37c5d60a1ee106d5157ded2538
saradepaulateixeira <saradepaulateixeira@gmail.com>
2024-07-16 14:45:01 -0300

    feat: Sintaxe de variáveis e indicações de uso

---
+++ Sintaxe da Linguagem/variaveis.php
@@ -0,0 +1,18 @@
+<?php
+
+   /* formas de declaração não permitidas:
+    $1forma = 1;
+    $!forma = 2;
+    $forma tres = 3;
+    $forma(quatro) = 4;
+    $forma+cinco = 5;
+   */
+
+    # por convenção a melhor forma é:
+    $formaUm = 1; # camel Case
+    $forma_dois = 2; # usando snake case
+
+    # Aceito mas não recomendado:
+    $FormaTres = 3; # studly case
+
+
